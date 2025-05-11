4db15c5c16532f3f4858af5e75a801e713051b78
Connie Lee <connielee@ConniedeMacBook-Air.local>
2025-05-11 23:40:16 +0800

    Update

--- Final_Project/app.py
+++ Final_Project/app.py
@@ -10,9 +10,9 @@ from agent_runner import run_agent_diaginosis, run_agent_recommendation
 from google_search import search_medication_location
 from pdf_generation import create_pdf
 
-# import os
-# os.system("playwright install")
-# os.system('playwright install-deps')
+import os
+os.system('apt-get update')
+os.system('apt-get install -y libpango1.0-0 libpangoft2-1.0-0 libharfbuzz-subset0 libjpeg-dev libopenjp2-7-dev libffi-dev')
 # 載入 .env 檔的環境變數（如 API 金鑰）
 # load_dotenv()
 gemini_api_key = st.secrets["GEMINI_API_KEY"]
