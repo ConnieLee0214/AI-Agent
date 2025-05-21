9da576a61a4272c938cdddb17f6d575100bea106
Connie Lee <connielee@ConniedeMacBook-Air.local>
2025-05-21 15:34:52 +0800

    Update

--- Final_Project/pdf_generation.py
+++ Final_Project/pdf_generation.py
@@ -5,15 +5,30 @@ from markupsafe import Markup
 # from weasyprint import HTML
 from xhtml2pdf import pisa
 import os
+import re
 
 
 def create_pdf(result_df):
+    def _break_chinese_paragraph(text, max_chars=40):
+        # 先用中文標點分句，加上 <br>（也支援英文句點）
+        text = re.sub(r'([。！？\?])', r'\1<br>', text)
+        # 若句子太長仍超出，額外加 <wbr> 每 max_chars 字
+        chunks = []
+        for line in text.split('<br>'):
+            if len(line) > max_chars:
+                chunks.extend([line[i:i+max_chars] + '<wbr>' for i in range(0, len(line), max_chars)])
+            else:
+                chunks.append(line)
+        return '<br>'.join(chunks)
+
     def _nl2br(value):
         return Markup(value.replace('\n', '<br>'))
     
     patient = result_df.iloc[0].to_dict()
-    # 載入模板並渲染
-    # env = Environment(loader=FileSystemLoader('/Users/connielee/Desktop/code/Final_Project'))
+    text_fields = ["可能疾病分析依據", "疫情新聞摘要", "是否與症狀有關聯", "參考來源", "嚴重程度評估", "是否需立即就醫"]
+    for field in text_fields:
+        if field in patient and isinstance(patient[field], str):
+            patient[field] = _break_chinese_paragraph(patient[field])
     template_dir = os.path.dirname(__file__)
     env = Environment(loader=FileSystemLoader(template_dir))
     env.filters['nl2br'] = _nl2br
@@ -21,8 +36,6 @@ def create_pdf(result_df):
     html_out = template.render(patient=patient)
 
     # 產生 PDF
-    # pdfkit.from_string(html_out, "/Users/connielee/Desktop/code/Final_Project/report.pdf")
-        # 使用 BytesIO 生成 PDF 並保存在內存中
     pdf_output = BytesIO()
     # pdfkit.from_string(html_out, pdf_output)
     # HTML(string=html_out).write_pdf(pdf_output)
